b9665a79ac335ecc65a4132c5178eef645698816
leon-ronaldo <leonronaldo.trs@gmail.com>
2024-08-24 09:45:49 +0530

    inital commit

---
+++ smart_staffs_app/android/app/src/main/java/com/visionaryVentures/smart_staffs_app/MainActivity.java
@@ -0,0 +1,6 @@
+package com.visionaryVentures.smart_staffs_app;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ smart_students_app/android/app/src/main/java/com/visionaryVentures/smart_students_app/MainActivity.java
@@ -0,0 +1,6 @@
+package com.visionaryVentures.smart_students_app;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
